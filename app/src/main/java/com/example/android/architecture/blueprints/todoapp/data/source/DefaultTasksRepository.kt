02d81833de718ac12bfd5f54bb067daab022396e
Minh Pham <pdminh0410@gmail.com>
2025-04-05 14:43:10 -0700

    Advanced Android in Kotlin 05.2:Introduction to Test Doubles and Dependency Injection - 9. Task: Make a ServiceLocator

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepository.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepository.kt
@@ -15,15 +15,10 @@
  */
 package com.example.android.architecture.blueprints.todoapp.data.source
 
-import android.app.Application
 import androidx.lifecycle.LiveData
-import androidx.room.Room
 import com.example.android.architecture.blueprints.todoapp.data.Result
 import com.example.android.architecture.blueprints.todoapp.data.Result.Success
 import com.example.android.architecture.blueprints.todoapp.data.Task
-import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksLocalDataSource
-import com.example.android.architecture.blueprints.todoapp.data.source.local.ToDoDatabase
-import com.example.android.architecture.blueprints.todoapp.data.source.remote.TasksRemoteDataSource
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
@@ -38,29 +33,6 @@ class DefaultTasksRepository(
     private val tasksLocalDataSource: TasksDataSource,
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : TasksRepository {
-
-    companion object {
-        @Volatile
-        private var INSTANCE: DefaultTasksRepository? = null
-
-        fun getRepository(app: Application): DefaultTasksRepository {
-            return INSTANCE ?: synchronized(this) {
-                val database = Room.databaseBuilder(
-                    context = app,
-                    klass = ToDoDatabase::class.java,
-                    name = "Tasks.db"
-                )
-                    .build()
-
-                DefaultTasksRepository(
-                    tasksRemoteDataSource = TasksRemoteDataSource,
-                    tasksLocalDataSource = TasksLocalDataSource(database.taskDao()),
-                ).also {
-                    INSTANCE = it
-                }
-            }
-        }
-    }
 
     override suspend fun getTasks(forceUpdate: Boolean): Result<List<Task>> {
         if (forceUpdate) {
